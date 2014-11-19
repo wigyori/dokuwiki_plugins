fc825b0b95fed909cdf147bdefe0d5ce6bdb868c
Daniel Stonier <d.stonier@gmail.com>
2014-11-19 14:04:25 +0900

    move away from svn structure layout.

--- html2pdf/tags/20090215/src/conf/default.php
+++
@@ -1,5 +0,0 @@
-<?php
-$conf['restrict']        =  true;      // restrict access 
-$conf['templatepath']    = 'lib/plugins/userhomepage/_template.txt';   // the location of the template file
-$conf['users_namespace'] = 'user' // namespace storing the user namespaces
-?>
--- html2pdf/tags/20090215/src/conf/metadata.php
+++
@@ -1,5 +0,0 @@
-<?php
-$meta['restrict']        = array('onoff');      // restrict access
-$meta['templatepath']    = array('string','_pattern' => '/^(|[a-zA-Z\-]+)$/'); // the location of the template file
-$meta['users_namespace'] = array('string','_pattern' => '/^(|[a-zA-Z\-:]+)$/'); // the namespace containing user directories 
-?>
